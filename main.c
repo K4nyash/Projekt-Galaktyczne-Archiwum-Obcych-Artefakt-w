c737e89fd008f64b494c07252af2fb2af582ec10
PB-MateuszC <95324@student.pb.edu.pl>
2026-01-07 21:43:36 +0100

    Główna pętla, usuwanie, modyfikacja

--- main.c
+++ main.c
@@ -25,7 +25,39 @@ struct Node* CreateNode(char name[100], char origin[100], char creatorCiv[100], 
     return newNode;
 }
 
-void Insert(struct Node** head, char name[100], char origin[100], char creatorCiv[100], int dangerLevel, int discoveryYear, char status[100]) {
+void Insert(struct Node** head) {
+    char name[100];
+    char origin[100];
+    char creatorCiv[100];
+    int dangerLevel;
+    int discoveryYear;
+    char status[100];
+
+    printf("Podaj nazwe artefaktu: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
+
+    printf("Podaj pochodzenie artefaktu: ");
+    fgets(origin, sizeof(origin), stdin);
+    origin[strcspn(origin, "\n")] = 0;
+
+    printf("Podaj cywilizacje tworcow artefaktu: ");
+    fgets(creatorCiv, sizeof(creatorCiv), stdin);
+    creatorCiv[strcspn(creatorCiv, "\n")] = 0;
+
+    printf("Podaj poziom zagrozenia artefaktu: ");
+    scanf("%d", &dangerLevel);
+    while ( getchar() != '\n' );
+
+    printf("Podaj rok odkrycia artefaktu: ");
+    scanf("%d", &discoveryYear);
+    while ( getchar() != '\n' );
+
+    printf("Podaj status artefaktu: ");
+    fgets(status, sizeof(status), stdin);
+    status[strcspn(status, "\n")] = 0;
+
+
     struct Node* newNode = CreateNode(name, origin, creatorCiv, dangerLevel, discoveryYear, status);
     if (*head == NULL) { 
         *head = newNode;
@@ -38,11 +70,121 @@ void Insert(struct Node** head, char name[100], char origin[100], char creatorCi
     temp->next = newNode;
 }
 
+void DeleteFromFirst(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = temp->next;
+    free(temp);
+}
+
+void Delete(struct Node** head) {
+
+    int position;
+
+    printf("Podaj pozycje do usuniecia: ");
+    scanf("%d", &position);
+    while ( getchar() != '\n' );
+
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    if (position == 0) {
+        DeleteFromFirst(head);
+        return;
+    }
+    for (int i = 0; temp != NULL && i < position - 1; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL || temp->next == NULL) {
+        printf("Position out of range\n");
+        return;
+    }
+    struct Node* next = temp->next->next;
+    free(temp->next);
+    temp->next = next;
+}
+
+void Update(struct Node** head) {
+
+    int position;
+    char name[100];
+    char origin[100];
+    char creatorCiv[100];
+    int dangerLevel;
+    int discoveryYear;
+    char status[100];
+
+    printf("Podaj pozycje do zmiany: ");
+    scanf("%d", &position);
+    while ( getchar() != '\n' );
+
+    printf("Podaj nazwe artefaktu: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
+
+    printf("Podaj pochodzenie artefaktu: ");
+    fgets(origin, sizeof(origin), stdin);
+    origin[strcspn(origin, "\n")] = 0;
+
+    printf("Podaj cywilizacje tworcow artefaktu: ");
+    fgets(creatorCiv, sizeof(creatorCiv), stdin);
+    creatorCiv[strcspn(creatorCiv, "\n")] = 0;
+
+    printf("Podaj poziom zagrozenia artefaktu: ");
+    scanf("%d", &dangerLevel);
+    while ( getchar() != '\n' );
+
+    printf("Podaj rok odkrycia artefaktu: ");
+    scanf("%d", &discoveryYear);
+    while ( getchar() != '\n' );
+
+    printf("Podaj status artefaktu: ");
+    fgets(status, sizeof(status), stdin);
+    status[strcspn(status, "\n")] = 0;
+
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    if (position == 0) {
+        strcpy(temp->name, name);
+        strcpy(temp->origin, origin);
+        strcpy(temp->creatorCiv, creatorCiv);
+        temp->dangerLevel = dangerLevel;
+        temp->discoveryYear = discoveryYear;
+        strcpy(temp->status, status);
+        return;
+    }
+    for (int i = 0; temp != NULL && i < position - 1; i++) {
+        temp = temp->next;
+    }
+    temp = temp->next;
+
+    if (temp == NULL || temp->next == NULL) {
+        printf("Position out of range\n");
+        return;
+    }
+    strcpy(temp->name, name);
+    strcpy(temp->origin, origin);
+    strcpy(temp->creatorCiv, creatorCiv);
+    temp->dangerLevel = dangerLevel;
+    temp->discoveryYear = discoveryYear;
+    strcpy(temp->status, status);
+
+}
+
+
 void Print(struct Node* head) {
     if (head == NULL){
         printf("Lista jest pusta");
     }
-    printf("%100s", "Nazwa");
+    printf("\n%100s", "Nazwa");
     printf("%30s", "Pochodzenie");
     printf("%50s", "Cywilizacja tworcow");
     printf("%10s", "Poz Zag");
@@ -61,13 +203,52 @@ void Print(struct Node* head) {
 }
 
 int main() {
-  struct Node* head = NULL;
-  // Temp
-
-  for(int i=0; i<10; i++){
-    Insert(&head, "Nazwa", "Pochodzenie", "Cywilizacja tworcow", 5, 2250, "bezpieczny");
-  }
-  Print(head);
+    struct Node* head = NULL;
+    char choice;
+    int isRunning = 1;
+    
+    while(isRunning == 1){
+        printf("\n=== GALAKTYCZNE ARCHIWUM OBCYCH ARTEFAKTOW (Rok 2471) ===\n");
+        printf("1. Rejestracja nowego artefaktu\n");
+        printf("2. Wyswietl katalog\n");
+        printf("3. Wyszukiwanie\n");
+        printf("4. Sortowanie\n");
+        printf("5. Edycja danych artefaktu\n");
+        printf("6. Usuniecie artefaktu\n");
+        printf("7. Zapisz zmiany do pliku\n");
+        printf("0. Wyjscie\n");
+        printf("=========================================================\n");
+        printf("Wybor: ");
+        choice = fgetc(stdin);
+        while ( getchar() != '\n' );
+        switch (choice)
+        {
+        case '1':
+            Insert(&head);
+            break;
+        case '2':
+            Print(head);
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            Update(&head);
+            break;
+        case '6':
+            Delete(&head);
+            break;
+        case '7':
+            break;
+        case '0':
+            isRunning = 0;
+            break;
+        default:
+            printf("Nieprawidlowy wybor\n");
+            break;
+        }
+    }
 
   return 0;
 } 
